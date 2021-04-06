4ff96bd10d61d8f6791dddc7070039df4106fef7
fri <fri>
2021-04-06 16:55:00 +0200

    Switched to Polyfuse as it seems more stable

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,16 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::ffi::{OsStr, OsString};
-use std::time::{Duration, UNIX_EPOCH};
-use libc::{ENOENT};
-use fuse::{FileType, FileAttr, Filesystem, Request, ReplyData, ReplyEntry, ReplyAttr, ReplyDirectory};
-use clap::{Arg, App};
+use polyfuse::{
+	op,
+	reply::{AttrOut, EntryOut, FileAttr, ReaddirOut, StatfsOut},
+	KernelConfig, Operation, Request, Session,
+};
+
+use anyhow::{ensure, Result, bail};
+use std::{io, os::unix::prelude::*, path::PathBuf, time::Duration};
+
+use clap::{Arg, App, crate_version};
 use std::os::unix::io::AsRawFd;
 use termios::{Termios, tcsetattr, ECHO, TCSANOW};
 
@@ -25,99 +30,151 @@ mod archive;
 
 use archive::{Archive, ArchiveEntry, ArchiveError};
 
-const TTL: Duration = Duration::from_secs(1);			// 1 second
+const TTL: Duration = Duration::from_secs(60 * 60 * 24 * 365);
+const ROOT_INO: u64 = 1;
+const HELLO_INO: u64 = 2;
+const HELLO_FILENAME: &str = "hello.txt";
+const HELLO_CONTENT: &[u8] = b"Hello, world!\n";
 
-const HELLO_DIR_ATTR: FileAttr = FileAttr {
-	ino: 1,
-	size: 0,
-	blocks: 0,
-	atime: UNIX_EPOCH,									// 1970-01-01 00:00:00
-	mtime: UNIX_EPOCH,
-	ctime: UNIX_EPOCH,
-	crtime: UNIX_EPOCH,
-	kind: FileType::Directory,
-	perm: 0o755,
-	nlink: 2,
-	uid: 501,
-	gid: 20,
-	rdev: 0,
-	flags: 0,
-};
+#[derive(Debug)]
+struct DirEntry {
+	name: String,
+	ino: u64,
+	typ: u32,
 
-const HELLO_TXT_CONTENT: &str = "Hello World!\n";
+	size: u64,
+	blksize: u32,
+	blocks: u64,
+	atime: Duration,
+	mtime: Duration,
+	ctime: Duration,
+	mode: u32,
+	nlink: u32,
+	uid: u32,
+	gid: u32,
+	rdev: u32,
+}
 
-const HELLO_TXT_ATTR: FileAttr = FileAttr {
-	ino: 2,
-	size: 13,
-	blocks: 1,
-	atime: UNIX_EPOCH,									// 1970-01-01 00:00:00
-	mtime: UNIX_EPOCH,
-	ctime: UNIX_EPOCH,
-	crtime: UNIX_EPOCH,
-	kind: FileType::RegularFile,
-	perm: 0o644,
-	nlink: 1,
-	uid: 501,
-	gid: 20,
-	rdev: 0,
-	flags: 0,
-};
+impl DirEntry {
+	fn fill_attr(&self, attr: &mut FileAttr) {
+		attr.ino(self.ino);
+		attr.size(self.size);
+		attr.blksize(self.blksize);
+		attr.blocks(self.blocks);
+		attr.atime(self.atime);
+		attr.mtime(self.mtime);
+		attr.ctime(self.ctime);
+		attr.mode(self.mode);
+		attr.nlink(self.nlink);
+		attr.uid(self.uid);
+		attr.gid(self.gid);
+		attr.rdev(self.rdev);
+	}
+}
 
 #[derive(Debug)]
 struct ArchiveFS {
 	archive_path: String,
 	password: Option<String>,
-	entries: Vec<FileAttr>,
+	entries: Vec<DirEntry>,
 }
 
-impl Filesystem for ArchiveFS {
-	fn lookup(&mut self, _req: &Request, parent: u64, name: &OsStr, reply: ReplyEntry) {
-		if parent == 1 && name.to_str() == Some("hello.txt") {
-			reply.entry(&TTL, &HELLO_TXT_ATTR, 0);
-		} else {
-			reply.error(ENOENT);
+impl ArchiveFS {
+	fn lookup(&self, req: &Request, op: op::Lookup<'_>) -> io::Result<()> {
+		match op.parent() {
+			ROOT_INO if op.name().as_bytes() == HELLO_FILENAME.as_bytes() => {
+				let mut out = EntryOut::default();
+				self.entries[2].fill_attr(out.attr());
+				out.ino(HELLO_INO);
+				out.ttl_attr(TTL);
+				out.ttl_entry(TTL);
+				req.reply(out)
+			}
+			_ => req.reply_error(libc::ENOENT),
 		}
 	}
 
-	fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
-		match ino {
-			1 => reply.attr(&TTL, &HELLO_DIR_ATTR),
-			2 => reply.attr(&TTL, &HELLO_TXT_ATTR),
-			_ => reply.error(ENOENT),
-		}
+	fn getattr(&self, req: &Request, op: op::Getattr<'_>) -> io::Result<()> {
+		let mut out = AttrOut::default();
+
+		match op.ino() {
+			ROOT_INO => self.entries[0].fill_attr(out.attr()),
+			HELLO_INO => self.entries[2].fill_attr(out.attr()),
+			_ => return req.reply_error(libc::ENOENT),
+		};
+
+		out.ttl(TTL);
+
+		req.reply(out)
 	}
 
-	fn read(&mut self, _req: &Request, ino: u64, _fh: u64, offset: i64, _size: u32, reply: ReplyData) {
-		if ino == 2 {
-			reply.data(&HELLO_TXT_CONTENT.as_bytes()[offset as usize..]);
-		} else {
-			reply.error(ENOENT);
+	fn read(&self, req: &Request, op: op::Read<'_>) -> io::Result<()> {
+		match op.ino() {
+			HELLO_INO => (),
+			ROOT_INO => return req.reply_error(libc::EISDIR),
+			_ => return req.reply_error(libc::ENOENT),
 		}
+
+		let mut data: &[u8] = &[];
+
+		let offset = op.offset() as usize;
+		if offset < HELLO_CONTENT.len() {
+			let size = op.size() as usize;
+			data = &HELLO_CONTENT[offset..];
+			data = &data[..std::cmp::min(data.len(), size)];
+		}
+
+		req.reply(data)
 	}
 
-	fn readdir(&mut self, _req: &Request, ino: u64, _fh: u64, offset: i64, mut reply: ReplyDirectory) {
-		if ino != 1 {
-			reply.error(ENOENT);
-			return;
+	fn dir_entries(&self) -> impl Iterator<Item = (u64, &DirEntry)> + '_ {
+		self.entries.iter().enumerate().map(|(i, ent)| {
+			let offset = (i + 1) as u64;
+			(offset, ent)
+		})
+	}
+
+	fn readdir(&self, req: &Request, op: op::Readdir<'_>) -> io::Result<()> {
+		if op.ino() != ROOT_INO {
+			return req.reply_error(libc::ENOTDIR);
 		}
 
-		let entries = vec![
-			(1, FileType::Directory, "."),
-			(1, FileType::Directory, ".."),
-			(2, FileType::RegularFile, "hello.txt"),
-		];
+		let mut out = ReaddirOut::new(op.size() as usize);
 
-		for (i, entry) in entries.into_iter().enumerate().skip(offset as usize) {
-			// i + 1 means the index of the next entry
-			reply.add(entry.0, (i + 1) as i64, entry.1, entry.2);
+		for (i, entry) in self.dir_entries().skip(op.offset() as usize) {
+			let full = out.entry(
+				entry.name.as_ref(), //
+				entry.ino,
+				entry.typ,
+				i + 1,
+			);
+			if full {
+				break;
+			}
 		}
-		reply.ok();
+
+		req.reply(out)
+	}
+
+	fn statfs(&self, req: &Request, _op: op::Statfs<'_>) -> io::Result<()> {
+		let mut out = StatfsOut::default();
+
+		out.statfs().bsize(512);
+		out.statfs().frsize(0);
+		out.statfs().blocks(0);
+		out.statfs().bfree(0);
+		out.statfs().bavail(0);
+		out.statfs().files(0);
+		out.statfs().ffree(0);
+		out.statfs().namelen(255);
+
+		req.reply(out)
 	}
 }
 
-fn main() {
+fn main() -> Result<()> {
 	let matches = App::new("archivemount-rs")
-		.version("0.1.0")
+		.version(crate_version!())
 		.author("Franco Bugnano")
 		.about("Mount an archive file as a read-only file system")
 		.arg(Arg::with_name("ARCHIVEPATH")
@@ -165,16 +222,51 @@ fn main() {
 		entries: Vec::new(),
 	};
 
+	fs.entries.push(DirEntry {
+		name: String::from("."),
+		ino: ROOT_INO,
+		typ: libc::DT_DIR as u32,
+
+		size: 0,
+		blksize: 512,
+		blocks: 0,
+		atime: Duration::new(0, 0),
+		mtime: Duration::new(0, 0),
+		ctime: Duration::new(0, 0),
+		mode: (libc::S_IFDIR as u32) | 0o555,
+		nlink: 2,
+		uid: unsafe { libc::getuid() },
+		gid: unsafe { libc::getgid() },
+		rdev: 0,
+	});
+	fs.entries.push(DirEntry {
+		name: String::from(".."),
+		ino: ROOT_INO,
+		typ: libc::DT_DIR as u32,
+
+		size: 0,
+		blksize: 512,
+		blocks: 0,
+		atime: Duration::new(0, 0),
+		mtime: Duration::new(0, 0),
+		ctime: Duration::new(0, 0),
+		mode: libc::S_IFDIR as u32 | 0o555,
+		nlink: 2,
+		uid: unsafe { libc::getuid() },
+		gid: unsafe { libc::getgid() },
+		rdev: 0,
+	});
+
 	let mut ino = 2;
 	let mut entry = ArchiveEntry::new();
-	let a = match Archive::read_open_filename(&fs.archive_path, 10240, None) {
+	let a = match Archive::read_open_filename(&fs.archive_path, 10240, fs.password.as_deref()) {
 		Ok(a) => a,
-		Err(ArchiveError::Eof) => panic!("EOF"),
-		Err(ArchiveError::Retry(s)) => panic!("{}", s),
-		Err(ArchiveError::Warn(s)) => panic!("{}", s),
-		Err(ArchiveError::Failed(s)) => panic!("{}", s),
-		Err(ArchiveError::Fatal(s)) => panic!("{}", s),
-		Err(ArchiveError::Unknown(_, s)) => panic!("{}", s),
+		Err(ArchiveError::Eof) => bail!("EOF"),
+		Err(ArchiveError::Retry(s)) => bail!("{}", s),
+		Err(ArchiveError::Warn(s)) => bail!("{}", s),
+		Err(ArchiveError::Failed(s)) => bail!("{}", s),
+		Err(ArchiveError::Fatal(s)) => bail!("{}", s),
+		Err(ArchiveError::Unknown(_, s)) => bail!("{}", s),
 	};
 
 	loop {
@@ -183,28 +275,29 @@ fn main() {
 				let st = entry.stat();
 				println!("{}", entry.pathname());
 				let kind = match st.st_mode & archive::AE_IFMT {
-					archive::AE_IFREG => FileType::RegularFile,
-					archive::AE_IFLNK => FileType::Symlink,
-					archive::AE_IFSOCK => FileType::Socket,
-					archive::AE_IFCHR => FileType::CharDevice,
-					archive::AE_IFBLK => FileType::BlockDevice,
-					archive::AE_IFDIR => FileType::Directory,
-					archive::AE_IFIFO => FileType::NamedPipe,
-					_ => FileType::RegularFile,
+					archive::AE_IFREG => libc::DT_REG,
+					archive::AE_IFLNK => libc::DT_LNK,
+					archive::AE_IFSOCK => libc::DT_SOCK,
+					archive::AE_IFCHR => libc::DT_CHR,
+					archive::AE_IFBLK => libc::DT_BLK,
+					archive::AE_IFDIR => libc::DT_DIR,
+					archive::AE_IFIFO => libc::DT_FIFO,
+					_ => libc::DT_REG,
 				};
 
-				let attr = FileAttr {
+				let attr = DirEntry {
+					name: entry.pathname(),
 					ino: ino,
+					typ: kind as u32,
 					size: st.st_size as u64,
+					blksize: 512,
 					blocks: ((st.st_size + 511) / 512) as u64,
-					atime: UNIX_EPOCH + Duration::new(st.st_atime as u64, st.st_atime_nsec as u32),
-					mtime: UNIX_EPOCH + Duration::new(st.st_mtime as u64, st.st_mtime_nsec as u32),
-					ctime: UNIX_EPOCH + Duration::new(st.st_ctime as u64, st.st_ctime_nsec as u32),
-					crtime: UNIX_EPOCH,
-					kind: kind,
-					perm: (st.st_mode & 0o7777) as u16,
+					atime: Duration::new(st.st_atime as u64, st.st_atime_nsec as u32),
+					mtime: Duration::new(st.st_mtime as u64, st.st_mtime_nsec as u32),
+					ctime: Duration::new(st.st_ctime as u64, st.st_ctime_nsec as u32),
+					mode: st.st_mode,
 					nlink: {
-						if (kind == FileType::Directory) && (st.st_nlink < 2) {
+						if (kind == libc::DT_DIR) && (st.st_nlink < 2) {
 							2
 						} else if st.st_nlink < 1 {
 							1
@@ -215,14 +308,12 @@ fn main() {
 					uid: st.st_uid,
 					gid: st.st_gid,
 					rdev: st.st_rdev as u32,
-					flags: 0,
 				};
+				println!("{:?}", attr);
+				println!("{}", st.st_mode);
 				fs.entries.push(attr);
 
 				ino += 1;
-
-				println!("{:?}", attr);
-				println!("{}", st.st_mode);
 			},
 			Err(e) => {
 				println!("{:?}", e);
@@ -231,23 +322,38 @@ fn main() {
 		}
 	}
 
-	let mut options: Vec<OsString> = Vec::new();
+	let mountpoint = PathBuf::from(matches.value_of("MOUNTPOINT").unwrap());
+    ensure!(mountpoint.is_dir(), "MOUNTPOINT must be a directory");
 
-	if let Some(o) = matches.values_of_os("o") {
+	let mut config = KernelConfig::default();
+	if let Some(o) = matches.values_of("o") {
 		for option in o {
-			options.push(OsString::from("-o"));
-			options.push(option.to_os_string());
+			config.mount_option(option);
 		}
 	}
 
-	let mountpoint = matches.value_of("MOUNTPOINT").unwrap();
-	let options = options
-		.iter()
-		.map(|o| o.as_ref())
-		.collect::<Vec<&OsStr>>();
+	let session = Session::mount(mountpoint, config)?;
 
-	println!("{:?}", options);
-	println!("{:?}", fs);
-	fuse::mount(fs, mountpoint, &options).unwrap();
+	// Per feature parity con fuse-rs:
+	// init (nop)
+	// destroy (nop)
+	// forget (nop)
+	// open (opened(0, 0))
+	// release (ok)
+	// opendir (opened(0, 0))
+	// releasedir (ok)
+	// statfs
+	while let Some(req) = session.next_request()? {
+		match req.operation()? {
+			Operation::Lookup(op) => fs.lookup(&req, op)?,
+			Operation::Getattr(op) => fs.getattr(&req, op)?,
+			Operation::Read(op) => fs.read(&req, op)?,
+			Operation::Readdir(op) => fs.readdir(&req, op)?,
+			Operation::Statfs(op) => fs.statfs(&req, op)?,
+			_ => req.reply_error(libc::ENOSYS)?,
+		}
+	}
+
+	Ok(())
 }
 
